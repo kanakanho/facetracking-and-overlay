22c86c087a4bacc4852add6201ada8b13a2a7175
kanakanho <kahoshibakichi@gmail.com>
2025-06-13 18:06:28 +0900

    add: 音声認識の処理を追加する場所について追記

--- facetracking-and-overlay/ExternalFileStorage/ExternalSentences.swift
+++ facetracking-and-overlay/ExternalFileStorage/ExternalSentences.swift
@@ -7,11 +7,17 @@
 
 import SwiftUI
 
+@MainActor
 /// 会話全体を格納して書き出す
 class ExternalSentences: ExternalFileStorage<ExternalPassage> {
     @Published private(set) var isVoiceRecognition: Bool = false
     
     func toggleVoiceRecognition() {
+        if isVoiceRecognition {
+            // 音声認識の終了処理
+        } else {
+            // 音声認識の開始処理
+        }
         isVoiceRecognition.toggle()
     }
 }
